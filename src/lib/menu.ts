d64d33703e78e8bc81bdc604e7dec514db0ec39f
Samin Sharar <saminsharar2@gmail.com>
2026-08-31 04:28:23 +0600

    :up: sidenav updated

--- src/lib/menu.ts
+++ src/lib/menu.ts
@@ -1,22 +1,10 @@
 import {
   Home,
-  Package,
-  ShoppingCart,
-  UserPlus,
-  User,
   CreditCard,
-  DollarSign,
   PlusCircle,
-  Clipboard,
-  BarChart2,
-  FolderPlus,
   Store,
-  Users,
   ShoppingBag,
-  Warehouse,
-  FilePlus2,
   Ticket,
-  Boxes,
   Inbox,
   LayoutList,
   BadgeCheck,
@@ -34,55 +22,6 @@ export interface MenuItem {
 
 export const sideMenu: MenuItem[] = [
   { title: "Dashboard", href: "/dashboard", icon: Home },
-
-  {
-    title: "Users",
-    icon: Users,
-    children: [
-      { title: "All Users", href: "/account/profile", icon: User },
-      { title: "Create Users", href: "/account/settings", icon: UserPlus },
-    ],
-  },
-
-  {
-    title: "Products",
-    icon: Package,
-    children: [
-      {
-        title: "Add Product",
-        href: "/dashboard/products/add-product",
-        icon: FilePlus2,
-      },
-      {
-        title: "Stock Update",
-        href: "/dashboard/products/stocks-update",
-        icon: Warehouse,
-      },
-      { title: "All Products", href: "/dashboard/products", icon: Boxes },
-      {
-        title: "All Categories",
-        href: "/dashboard/products/view-category",
-        icon: FolderPlus,
-      },
-    ],
-  },
-
-  {
-    title: "Orders",
-    icon: ShoppingCart,
-    children: [
-      {
-        title: "Create Order",
-        href: "/dashboard/orders/create-order",
-        icon: PlusCircle,
-      },
-      {
-        title: "All Orders",
-        href: "/dashboard/orders/view-order",
-        icon: Clipboard,
-      },
-    ],
-  },
 
   {
     title: "Store",
@@ -123,14 +62,6 @@ export const sideMenu: MenuItem[] = [
     ],
   },
 
-  {
-    title: "Financial",
-    icon: DollarSign,
-    children: [
-      { title: "Billing", href: "/financial/billing", icon: CreditCard },
-      { title: "Cost", href: "/financial/cost", icon: BarChart2 },
-    ],
-  },
   {
     title: "Requests",
     icon: Ticket,
